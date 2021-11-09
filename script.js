77b6eac22ce712f7e797b42779ec4c631b441d32
farhan-sqb <91463169+farhan-sqb@users.noreply.github.com>
2021-11-09 10:28:52 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('img').forEach(item => {
     item.addEventListener('click', event => {
-        console.log(item);
         window.open(item.src, '_blank').focus();
     });
 });
