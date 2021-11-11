e3930d9a4063190bb8d743ff28fd9e0d7017ce4b
farhan-sqb <91463169+farhan-sqb@users.noreply.github.com>
2021-11-11 08:14:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ document.querySelectorAll('img').forEach(item => {
         window.open(item.src, '_blank').focus();
     });
 });
+
+document.body.addEventListener('keydown', function(e){
+    let currentImage = document.getElementById('currentImage').value;
+    if(currentImage == 12)
+        currentImage = 0;
+    
+    if(currentImage == -1)
+        currentImage = 0;
+
+    if(e.key == "ArrowRight"){
+        document.getElementsByTagName('img')[currentImage].style.border = '3px solid red';
+        currentImage++;
+        
+        document.getElementById('currentImage').value = currentImage;
+    }
+
+    if(e.key == "ArrowLeft"){
+        currentImage--;
+        document.getElementsByTagName('img')[currentImage].style.border = '0px';
+        
+        document.getElementById('currentImage').value = currentImage;
+    }
+});
